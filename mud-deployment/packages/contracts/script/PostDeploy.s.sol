8e39920cb352b63c5b5a98ee702799a39640b4fa
0xhatsume <0xhatsume@gmail.com>
2024-09-22 07:48:59 +0800

    :sparkles: Deploy contracts

--- mud-deployment/packages/contracts/script/PostDeploy.s.sol
+++ mud-deployment/packages/contracts/script/PostDeploy.s.sol
@@ -13,63 +13,63 @@ import { positionToEntityKey } from "../src/positionToEntityKey.sol";
 contract PostDeploy is Script {
   function run(address worldAddress) external {
     // Specify a store so that you can use tables directly in PostDeploy
-    StoreSwitch.setStoreAddress(worldAddress);
+    // StoreSwitch.setStoreAddress(worldAddress);
 
-    // Load the private key from the `PRIVATE_KEY` environment variable (in .env)
-    uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+    // // Load the private key from the `PRIVATE_KEY` environment variable (in .env)
+    // uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
 
-    vm.startBroadcast(deployerPrivateKey);
+    // vm.startBroadcast(deployerPrivateKey);
 
-    TerrainType O = TerrainType.None;
-    TerrainType T = TerrainType.TallGrass;
-    TerrainType B = TerrainType.Boulder;
+    // TerrainType O = TerrainType.None;
+    // TerrainType T = TerrainType.TallGrass;
+    // TerrainType B = TerrainType.Boulder;
 
-    TerrainType[20][20] memory map = [
-      [O, O, O, O, O, O, T, O, O, O, O, O, O, O, O, O, O, O, O, O],
-      [O, O, T, O, O, O, O, O, T, O, O, O, O, B, O, O, O, O, O, O],
-      [O, T, T, T, T, O, O, O, O, O, O, O, O, O, O, T, T, O, O, O],
-      [O, O, T, T, T, T, O, O, O, O, B, O, O, O, O, O, T, O, O, O],
-      [O, O, O, O, T, T, O, O, O, O, O, O, O, O, O, O, O, T, O, O],
-      [O, O, O, B, B, O, O, O, O, O, O, O, O, O, O, O, O, O, O, O],
-      [O, T, O, O, O, B, B, O, O, O, O, T, O, O, O, O, O, B, O, O],
-      [O, O, T, T, O, O, O, O, O, T, O, B, O, O, T, O, B, O, O, O],
-      [O, O, T, O, O, O, O, T, T, T, O, B, B, O, O, O, O, O, O, O],
-      [O, O, O, O, O, O, O, T, T, T, O, B, T, O, T, T, O, O, O, O],
-      [O, B, O, O, O, B, O, O, T, T, O, B, O, O, T, T, O, O, O, O],
-      [O, O, B, O, O, O, T, O, T, T, O, O, B, T, T, T, O, O, O, O],
-      [O, O, B, B, O, O, O, O, T, O, O, O, B, O, T, O, O, O, O, O],
-      [O, O, O, B, B, O, O, O, O, O, O, O, O, B, O, T, O, O, O, O],
-      [O, O, O, O, B, O, O, O, O, O, O, O, O, O, O, O, O, O, O, O],
-      [O, O, O, O, O, O, O, O, O, O, B, B, O, O, T, O, O, O, O, O],
-      [O, O, O, O, T, O, O, O, T, B, O, O, O, T, T, O, B, O, O, O],
-      [O, O, O, T, O, T, T, T, O, O, O, O, O, T, O, O, O, O, O, O],
-      [O, O, O, T, T, T, T, O, O, O, O, T, O, O, O, T, O, O, O, O],
-      [O, O, O, O, O, T, O, O, O, O, O, O, O, O, O, O, O, O, O, O]
-    ];
+    // TerrainType[20][20] memory map = [
+    //   [O, O, O, O, O, O, T, O, O, O, O, O, O, O, O, O, O, O, O, O],
+    //   [O, O, T, O, O, O, O, O, T, O, O, O, O, B, O, O, O, O, O, O],
+    //   [O, T, T, T, T, O, O, O, O, O, O, O, O, O, O, T, T, O, O, O],
+    //   [O, O, T, T, T, T, O, O, O, O, B, O, O, O, O, O, T, O, O, O],
+    //   [O, O, O, O, T, T, O, O, O, O, O, O, O, O, O, O, O, T, O, O],
+    //   [O, O, O, B, B, O, O, O, O, O, O, O, O, O, O, O, O, O, O, O],
+    //   [O, T, O, O, O, B, B, O, O, O, O, T, O, O, O, O, O, B, O, O],
+    //   [O, O, T, T, O, O, O, O, O, T, O, B, O, O, T, O, B, O, O, O],
+    //   [O, O, T, O, O, O, O, T, T, T, O, B, B, O, O, O, O, O, O, O],
+    //   [O, O, O, O, O, O, O, T, T, T, O, B, T, O, T, T, O, O, O, O],
+    //   [O, B, O, O, O, B, O, O, T, T, O, B, O, O, T, T, O, O, O, O],
+    //   [O, O, B, O, O, O, T, O, T, T, O, O, B, T, T, T, O, O, O, O],
+    //   [O, O, B, B, O, O, O, O, T, O, O, O, B, O, T, O, O, O, O, O],
+    //   [O, O, O, B, B, O, O, O, O, O, O, O, O, B, O, T, O, O, O, O],
+    //   [O, O, O, O, B, O, O, O, O, O, O, O, O, O, O, O, O, O, O, O],
+    //   [O, O, O, O, O, O, O, O, O, O, B, B, O, O, T, O, O, O, O, O],
+    //   [O, O, O, O, T, O, O, O, T, B, O, O, O, T, T, O, B, O, O, O],
+    //   [O, O, O, T, O, T, T, T, O, O, O, O, O, T, O, O, O, O, O, O],
+    //   [O, O, O, T, T, T, T, O, O, O, O, T, O, O, O, T, O, O, O, O],
+    //   [O, O, O, O, O, T, O, O, O, O, O, O, O, O, O, O, O, O, O, O]
+    // ];
 
-    uint32 height = uint32(map.length);
-    uint32 width = uint32(map[0].length);
-    bytes memory terrain = new bytes(width * height);
+    // uint32 height = uint32(map.length);
+    // uint32 width = uint32(map[0].length);
+    // bytes memory terrain = new bytes(width * height);
 
-    for (uint32 y = 0; y < height; y++) {
-      for (uint32 x = 0; x < width; x++) {
-        TerrainType terrainType = map[y][x];
-        if (terrainType == TerrainType.None) continue;
+    // for (uint32 y = 0; y < height; y++) {
+    //   for (uint32 x = 0; x < width; x++) {
+    //     TerrainType terrainType = map[y][x];
+    //     if (terrainType == TerrainType.None) continue;
 
-        terrain[(y * width) + x] = bytes1(uint8(terrainType));
+    //     terrain[(y * width) + x] = bytes1(uint8(terrainType));
 
-        bytes32 entity = positionToEntityKey(int32(x), int32(y));
-        if (terrainType == TerrainType.Boulder) {
-          Position.set(entity, int32(x), int32(y));
-          Obstruction.set(entity, true);
-        } else if (terrainType == TerrainType.TallGrass) {
-          Position.set(entity, int32(x), int32(y));
-          EncounterTrigger.set(entity, true);
-        }
-      }
-    }
+    //     bytes32 entity = positionToEntityKey(int32(x), int32(y));
+    //     if (terrainType == TerrainType.Boulder) {
+    //       Position.set(entity, int32(x), int32(y));
+    //       Obstruction.set(entity, true);
+    //     } else if (terrainType == TerrainType.TallGrass) {
+    //       Position.set(entity, int32(x), int32(y));
+    //       EncounterTrigger.set(entity, true);
+    //     }
+    //   }
+    // }
 
-    MapConfig.set(width, height, terrain);
+    // MapConfig.set(width, height, terrain);
 
     vm.stopBroadcast();
   }
